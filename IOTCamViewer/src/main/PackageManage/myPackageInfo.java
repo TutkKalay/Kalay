dd2eeb564f922b57e48bf685596421c7874d8d27
Neo <mingtseng.chang@huafu.com.tw>
2017-03-17 20:49:39 +0800

    First commit

---
+++ IOTCamViewer/src/main/PackageManage/myPackageInfo.java
@@ -0,0 +1,10 @@
+package PackageManage;
+
+/**
+ * Created by James Huang on 2015/3/26.
+ */
+public class myPackageInfo {
+    public final static String KALAY_CAM = "com.tutk.P2PCamLive.Kalay";
+    public final static String KALAY_CAR = "com.tutk.Kalay.Novatek";
+    public final static String KALAY_VSAAS = "com.tutk.Kalay.VSaaS";
+}
